81302a4007c9deb0f965f41ae3437f04acecdfa5
Teppei Sato <teppeis@gmail.com>
2018-10-24 21:30:13 +0900

    docs: update comment

--- src/closure-module-b.js
+++ src/closure-module-b.js
@@ -1,8 +1,8 @@
-// Closure Modules can load Closure Modules, Closure Scripts and ES Modules with declareModuleId()
+// Closure Modules can load Closure Scripts, Closure Modules and ES Modules with goog.declareModuleId()
 goog.module('closuremodule.b');
 
-const moduleA = goog.require('closuremodule.a');
 const scriptA = goog.require('closurescript.a');
+const moduleA = goog.require('closuremodule.a');
 const esmA = goog.require('esm.a');
 
-console.log(moduleA, scriptA, esmA);
+console.log(scriptA, moduleA, esmA);
